899812a7e477c154f361acdd22ae089fc4ff2c31
BOYSABIO <129614198+BOYSABIO@users.noreply.github.com>
2023-10-09 11:47:29 +0200

    Pandas Practice
    Intro to pandas, Series, DataFrame, and CSV reading

--- OTHER/LEETCODE/2887_Datafram_From_List.py
+++ OTHER/LEETCODE/2887_Datafram_From_List.py
@@ -21,3 +21,24 @@ print(df)
 # df.rename(columns = {'0':'Student_ID'})
 df.columns = ["Student_id", "Age"]
 print(df)
+
+
+student_data2 = {
+    "student_id":[1, 2, 3, 4, 5],
+    "Age":[10, 20, 50, 18, 75]
+}
+
+result_student_data2 = pd.DataFrame(student_data2)
+print(result_student_data2)
+
+
+x = "Yes"
+student_id_list = []
+
+# while x == "Yes":
+    #new_student = 1
+    #student_id_list.append(new_student)
+    #new_student_age = int(input("Please enter student age: "))
+
+
+print(df.size(student_data))
